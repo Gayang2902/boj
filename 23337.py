7e680dc4ef9f70373f396a553be82ac96f003a0e
K_DOUK03 <binaryk01@naver.com>
2024-07-01 01:04:30 +0900

    drunk passenger problem

--- 23337.py
+++ 23337.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
-from random import randint
-
-def test(n: int) -> float: 
-    seat = [i for i in range(n)]
-    occupied = [False] * n
+"""""""""
+drunk passenger 문제에서 n번 승객의 자리를 누군가가 이미
+차지하고 있을 확률을 구하는 코드 
+"""""""""
+def solve(n: int) -> float:
+    ans: float = 1
     
-    occupied[randint(1, n)] = True
-    for i in range(1, n):
-        
-        
-
-def main():
-    n = int(input())
-    print(test(n))
+    if n != 2:
+        # 1번 승객이 n번 승객 자리를 고를 확률
+        ans = 1.0 / (n - 1)
+        ans += ((n - 2) / (n - 1)) / 2
     
-if __name__ == '__main__':
-    main()
+    return ans
+
+n = int(input())
+print(f"{solve(n):.15f}")
